bac3761a667aa205831bc64c247e1fc48510bd11
Derek Emmanuel <derekemmanuel99@gmail.com>
2025-06-16 21:53:19 +0100

    added frontend domain

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "telehealth-frontend.vercel.app",
+      "https://telehealth-frontend.vercel.app",
     ], // frontend origin
     credentials: true, // required to receive/set cookies
   })
